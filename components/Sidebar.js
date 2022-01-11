5e150b15a823f0458c63217ff895a87d03346e24
Igor Vereschinsky <testdeveloper7967@gmail.com>
2022-01-11 23:16:56 +0300

    Complete app

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,10 @@
-import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon} from "@heroicons/react/outline";
+import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, RssIcon} from "@heroicons/react/outline";
 import {signOut, useSession} from 'next-auth/react';
 import {useEffect, useState} from "react";
 import useSpotify from "../hooks/useSpotify";
 import {useRecoilState} from "recoil";
 import {playlistIdState} from "../atoms/playlistAtom";
+import {HeartIcon} from "@heroicons/react/solid";
 
 function Sidebar(props) {
     const spotifyApi = useSpotify();
@@ -43,7 +44,7 @@ function Sidebar(props) {
                     <p>Create Playlist</p>
                 </button>
                 <button className={'flex items-center space-x-2 hover:text-white'}>
-                    <HeartIcon className={'h-5 w-5'}/>
+                    <HeartIcon className={'h-5 w-5 text-blue-500'}/>
                     <p>Liked Songs</p>
                 </button>
                 <button className={'flex items-center space-x-2 hover:text-white'}>
