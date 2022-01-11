a5688b7244b20df9e4a35021b09b0744a0b6db07
Igor Vereschinsky <testdeveloper7967@gmail.com>
2022-01-11 22:53:45 +0300

    Building Center of the Player component

--- components/Player.js
+++ components/Player.js
@@ -4,6 +4,16 @@ import {useRecoilState} from "recoil";
 import {currentTrackIdState, isPlayingState} from "../atoms/songAtom";
 import {useEffect, useState} from "react";
 import useSongInfo from "../hooks/useSongInfo";
+import {HeartIcon, VolumeUpIcon as VolumeDownIcon} from "@heroicons/react/outline";
+import {
+    RewindIcon,
+    SwitchHorizontalIcon,
+    FastForwardIcon,
+    PauseIcon,
+    PlayIcon,
+    ReplyIcon,
+    VolumeUpIcon
+} from "@heroicons/react/solid";
 
 function Player(props) {
     const spotifyApi = useSpotify();
@@ -27,6 +37,18 @@ function Player(props) {
         }
     };
 
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+            if (data.body.is_playing) {
+                spotifyApi.pause();
+                setIsPlaying(false);
+            } else {
+                spotifyApi.play();
+                setIsPlaying(true);
+            }
+        });
+    };
+
     useEffect(() => {
         if (spotifyApi.getAccessToken() && !currentTrackId) {
             fetchCurrentSong();
@@ -35,13 +57,30 @@ function Player(props) {
     }, [currentTrackId, spotifyApi, session]);
 
     return (
-        <div className={"h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8"}>
+        <div
+            className={"h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8"}>
             <div className={"flex items-center space-x-4"}>
                 <img className={"hidden md:inline h-10 w-10"} src={songInfo?.album.images?.[0]?.url} alt=""/>
                 <div>
                     <h3>{songInfo?.name}</h3>
                     <p>{songInfo?.artists?.[0]?.name}</p>
                 </div>
+            </div>
+
+            <div className={"flex items-center justify-evenly"}>
+                <SwitchHorizontalIcon className={"button"}/>
+                <RewindIcon className={"button"}/>
+                {isPlaying ? (
+                    <PauseIcon onClick={handlePlayPause} className={"button w-10 h-10"}/>
+                ) : (
+                    <PlayIcon onClick={handlePlayPause} className={"button w-10 h-10"}/>
+                )}
+                <FastForwardIcon className={"button"}/>
+                <ReplyIcon className={"button"}/>
+            </div>
+
+            <div>
+
             </div>
         </div>
     );
